ca345424ea39b3e5d1762c9dd29cd37572101dbc
aashirisrar <aashirisrar968@gmail.com>
2022-12-29 04:34:37 +0500

    created functions in file

--- LexicographicallyMinimum.cpp
+++ LexicographicallyMinimum.cpp
@@ -2,7 +2,55 @@
 #include <cstring>
 using namespace std;
 
-void Sort2DArrayAlphabetically(char leftRotatedCombinations[][10], int lenOfArr)
+void RotateLeft(char str[]);
+void Sort2DArrayAlphabetically(char leftRotatedCombinations[][50], int lenOfArr);
+
+void LeftRotateArray(char arr[], char leftRotatedCombinations[][50], int lenOfArr);
+
+int main()
+{
+    char arr[50];
+    char leftRotatedCombinations[50][50];
+
+    cout << "Enter a string:";
+    cin >> arr;
+
+    int lenOfArr = strlen(arr);
+
+    for (int i = 0; i < lenOfArr; i++)
+    {
+        RotateLeft(arr);
+        strcpy(leftRotatedCombinations[i], arr);
+    }
+
+    // This code will give all the possible left rotations and print them
+    // LeftRotateArray(arr, leftRotatedCombinations, lenOfArr);
+
+    Sort2DArrayAlphabetically(leftRotatedCombinations, lenOfArr);
+
+    cout << "The output is:" << endl;
+    cout << leftRotatedCombinations[0];
+
+    return 0;
+}
+
+void RotateLeft(char str[])
+{
+    // Left Rotation
+    int lenOfArr = strlen(str);
+    char temp = str[0];
+    int i;
+    for (i = 1; i < lenOfArr; i++)
+    {
+        char temporary = str[i + 1];
+        str[i - 1] = str[i];
+        str[i] = temporary;
+    }
+
+    str[i - 1] = temp;
+}
+
+void Sort2DArrayAlphabetically(char leftRotatedCombinations[][50], int lenOfArr)
 {
     char temp[100];
 
@@ -20,7 +68,7 @@ void Sort2DArrayAlphabetically(char leftRotatedCombinations[][10], int lenOfArr)
     }
 }
 
-void LeftRotateArray(char arr[], char leftRotatedCombinations[][10], int lenOfArr)
+void LeftRotateArray(char arr[], char leftRotatedCombinations[][50], int lenOfArr)
 {
     cout << "The following are all of the combinations by the left rotation:" << endl;
     for (int m = 0; m < lenOfArr; m++)
@@ -48,24 +96,4 @@ void LeftRotateArray(char arr[], char leftRotatedCombinations[][10], int lenOfAr
         }
         cout << endl;
     }
-}
-
-int main()
-{
-    char arr[10];
-    char leftRotatedCombinations[10][10];
-
-    cout << "Enter a string:";
-    cin >> arr;
-
-    int lenOfArr = strlen(arr);
-
-    LeftRotateArray(arr, leftRotatedCombinations, lenOfArr);
-
-    Sort2DArrayAlphabetically(leftRotatedCombinations, lenOfArr);
-
-    cout << "The output is:" << endl;
-    cout << leftRotatedCombinations[0];
-
-    return 0;
 }
