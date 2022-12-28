848e00e8ba5fd9676ac907c7ca0c08f3946a82e1
aashirisrar <aashirisrar968@gmail.com>
2022-12-29 04:25:01 +0500

    created the lexicographic minimum algorithm

---
+++ LexicographicallyMinimum.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void Sort2DArrayAlphabetically(char leftRotatedCombinations[][10], int lenOfArr)
+{
+    char temp[100];
+
+    for (int i = 0; i < lenOfArr; i++)
+    {
+        for (int j = i + 1; j < lenOfArr - 1; j++)
+        {
+            if (strcmp(leftRotatedCombinations[i], leftRotatedCombinations[j]) > 0)
+            {
+                strcpy(temp, leftRotatedCombinations[j]);
+                strcpy(leftRotatedCombinations[j], leftRotatedCombinations[i]);
+                strcpy(leftRotatedCombinations[i], temp);
+            }
+        }
+    }
+}
+
+void LeftRotateArray(char arr[], char leftRotatedCombinations[][10], int lenOfArr)
+{
+    cout << "The following are all of the combinations by the left rotation:" << endl;
+    for (int m = 0; m < lenOfArr; m++)
+    {
+        // Left Rotation
+        char temp = arr[0];
+        int i;
+        for (i = 1; i < lenOfArr; i++)
+        {
+            char temporary = arr[i + 1];
+            arr[i - 1] = arr[i];
+            arr[i] = temporary;
+        }
+
+        arr[i - 1] = temp;
+
+        for (int k = 0; k < lenOfArr; k++)
+        {
+            leftRotatedCombinations[m][k] = arr[k];
+        }
+
+        for (int a = 0; a < lenOfArr; a++)
+        {
+            cout << leftRotatedCombinations[m][a];
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    char arr[10];
+    char leftRotatedCombinations[10][10];
+
+    cout << "Enter a string:";
+    cin >> arr;
+
+    int lenOfArr = strlen(arr);
+
+    LeftRotateArray(arr, leftRotatedCombinations, lenOfArr);
+
+    Sort2DArrayAlphabetically(leftRotatedCombinations, lenOfArr);
+
+    cout << "The output is:" << endl;
+    for (int i = 0; i < lenOfArr; i++)
+    {
+        cout << leftRotatedCombinations[0][i];
+    }
+
+    return 0;
+}
